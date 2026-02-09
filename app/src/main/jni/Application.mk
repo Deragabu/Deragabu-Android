6e26faff267713e45513f1054b49ff98c1176120
qwe7002 <git@qwe7002.dev>
2026-02-09 04:23:32 -0800

    chore: update minimum SDK to 34; restrict support to 64-bit ARM architecture

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,7 +1,11 @@
 # Application.mk for Moonlight
 
 # Our minimum version is Android 5.0
-APP_PLATFORM := android-21
+APP_PLATFORM := android-34
 
 # We support 16KB pages
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
+
+# Only support 64-bit ARM architecture (remove armv7 and x86 support)
+APP_ABI := arm64-v8a
+
